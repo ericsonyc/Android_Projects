f74f20cad36f6e1adb36af57e462e44459f61632
ericsonyc <1220246394@qq.com>
2015-12-06 14:18:13 +0800

    commit ActivityLifeCycle project

--- ActivityLifeCycle/app/src/main/java/com/android/activitylifecycle/app/OrientationActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/android/activitylifecycle/app/OrientationActivity.java
@@ -1,16 +1,86 @@
 package com.android.activitylifecycle.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class OrientationActivity extends AppCompatActivity {
+public class OrientationActivity extends Activity {
+
+    private static final String TAG = "OrientationActivity";
+    private int param = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orientation);
+        Log.i(TAG, "onCreate called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("param", param);
+        Log.i(TAG, "onSaveInstanceState called, put param " + param);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        param = savedInstanceState.getInt("param");
+        Log.i(TAG, "onRestoreInstanceState called. get param " + param);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i(TAG, "onConfigurationChanged called");
+        switch (newConfig.orientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                setContentView(R.layout.orientation_portrait);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setContentView(R.layout.orientation_landscape);
+                break;
+        }
     }
 
     @Override
