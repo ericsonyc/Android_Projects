f986cc1a7c919bd43a2580c2096f0e5fd8d5b822
ericsonyc <1220246394@qq.com>
2015-12-07 17:55:13 +0800

    commit a new project

---
+++ AndroidExercises/app/src/main/java/com/android/androidexercises/app/SmartImageView.java
@@ -0,0 +1,42 @@
+package com.android.androidexercises.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class SmartImageView extends ImageView {
+
+    private float ratio = 2.43f;
+
+    public SmartImageView(Context context) {
+        super(context);
+    }
+
+    public SmartImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmartImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec) - getPaddingLeft() - getPaddingRight();
+        int height = MeasureSpec.getSize(heightMeasureSpec) - getPaddingTop() - getPaddingBottom();
+        if (widthMode == MeasureSpec.EXACTLY && heightMode != MeasureSpec.EXACTLY && ratio != 0.0f) {
+            height = (int) (width / ratio + 0.5f);
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        } else if (widthMode != MeasureSpec.EXACTLY && heightMode == MeasureSpec.EXACTLY && ratio != 0.0f) {
+            width = (int) (height * ratio + 0.5f);
+            widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
