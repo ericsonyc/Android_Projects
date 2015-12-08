3729d13dc12a25b9ba48221bdeb98cb84c305068
ericsonyc <1220246394@qq.com>
2015-12-08 20:33:24 +0800

    add all

---
+++ AndroidExercises/app/src/main/java/com/android/androidexercises/app/SlideWindowActivity.java
@@ -0,0 +1,7 @@
+package com.android.androidexercises.app;
+
+/**
+ * Created by ericson on 2015/12/8 0008.
+ */
+public class SlideWindowActivity {
+}
--- AndroidExercises/app/src/main/java/com/android/androidexercises/app/TouchEventChilds.java
+++ AndroidExercises/app/src/main/java/com/android/androidexercises/app/TouchEventChilds.java
@@ -1,41 +1,40 @@
 package com.android.androidexercises.app;
 
+/**
+ * Created by ericson on 2015/12/8 0008.
+ */
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
-/**
- * TODO: document your custom view class.
- */
-public class TouchEventFather extends LinearLayout {
+public class TouchEventChilds extends LinearLayout {
 
     private static final String TAG = "TouchEvent";
 
-    public TouchEventFather(Context context) {
+    public TouchEventChilds(Context context) {
         super(context);
     }
 
-    public TouchEventFather(Context context, AttributeSet attrs) {
+    public TouchEventChilds(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        Log.i(TAG, "TouchEventFather|dispatchTouchEvent-->" + TouchEventUtil.getTouchAction(ev.getAction()));
+        Log.e(TAG, "TouchEventChilds | dispatchTouchEvent --> " + TouchEventUtil.getTouchAction(ev.getAction()));
         return super.dispatchTouchEvent(ev);
     }
 
-    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        Log.i(TAG, "TouchEventFather | onInterceptTouchEvent --> " + TouchEventUtil.getTouchAction(ev.getAction()));
+        Log.i(TAG, "TouchEventChilds | onInterceptTouchEvent --> " + TouchEventUtil.getTouchAction(ev.getAction()));
         return super.onInterceptTouchEvent(ev);
     }
 
-    @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        Log.d(TAG, "TouchEventFather | onTouchEvent --> " + TouchEventUtil.getTouchAction(ev.getAction()));
+        Log.d(TAG, "TouchEventChilds | onTouchEvent --> " + TouchEventUtil.getTouchAction(ev.getAction()));
         return super.onTouchEvent(ev);
     }
+
 }
