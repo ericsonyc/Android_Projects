2fab7021ef1665cfacf2ca973220c2cc2c95d10d
ericsonyc <1220246394@qq.com>
2015-12-10 18:45:47 +0800

    commit alter

--- Advertisement/app/src/main/java/com/android/advertisement/app/MainActivity.java
+++ Advertisement/app/src/main/java/com/android/advertisement/app/MainActivity.java
@@ -2,8 +2,8 @@ package com.android.advertisement.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.LinearLayout;
+import cn.waps.AppConnect;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.id_layout);
+        AppConnect.getInstance(this).showBannerAd(this, layout);
     }
 }
