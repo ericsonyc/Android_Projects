a5b4fb0c73bb5c33c5fd3d9f781306f496d183b8
ericsonyc <1220246394@qq.com>
2015-12-12 09:34:56 +0800

    add all

--- Compass/app/src/main/java/com/android/compass/app/CompassView.java
+++ Compass/app/src/main/java/com/android/compass/app/CompassView.java
@@ -1,9 +1,14 @@
 package com.android.compass.app;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -11,18 +16,107 @@ import android.view.View;
  */
 public class CompassView extends View {
 
+    private static String TAG = "Compass";
+
     private Paint circlePaint, tickPaint;
+    private TextPaint textPaint;
+    private float vWidth, vHeight;
+    private float compassRadius;
+    private float tickLength;
+    private float textHeight, textWidth;
 
     public CompassView(Context context) {
         super(context);
+        Activity main = (Activity) context;
+        DisplayMetrics dm = new DisplayMetrics();
+        main.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Log.i(TAG, "width:" + dm.widthPixels);
+        Log.i(TAG, "height:" + dm.heightPixels);
+        initPaint(context);
     }
 
     public CompassView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initPaint(context);
+    }
+
+    private void initPaint(Context context) {
+        //对圆盘画初始化
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+        circlePaint.setColor(Color.BLACK);
+        circlePaint.setStyle(Paint.Style.FILL);
+
+        //对刻度画笔进行初始化
+        tickPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        tickPaint.setColor(Color.RED);
+        tickPaint.setStrokeWidth(3);
+
+        //对字的画笔进行初始化
+        textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(Color.GREEN);
+        textPaint.setTextSize(20);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        vWidth = w;
+        vHeight = h;
+        compassRadius = Math.min(w, h) * 0.75f / 2;
+        tickLength = (1 / 8f) * compassRadius;
+        textHeight = textPaint.descent() - textPaint.ascent();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+//        canvas.drawColor(Color.CYAN);
+        canvas.drawCircle(compassRadius, compassRadius, compassRadius, circlePaint);
+        int degress;
+        float textWidth;
+        for (int i = 0; i < 24; i++) {
+            canvas.save();
+            canvas.translate(compassRadius, compassRadius);
+            degress = i * 15;
+            canvas.rotate(15 * i);
+            canvas.drawLine(0, -compassRadius, 0, -compassRadius + tickLength, tickPaint);
+            switch (degress) {
+                case 0:
+                    textWidth = textPaint.measureText("45");
+                    drawText(canvas, "45", textWidth);
+                    break;
+                case 45:
+                    textWidth = textPaint.measureText("东");
+                    drawText(canvas, "东", textWidth);
+                    break;
+                case 90:
+                    textWidth = textPaint.measureText("135");
+                    drawText(canvas, "135", textWidth);
+                    break;
+                case 135:
+                    textWidth = textPaint.measureText("南");
+                    drawText(canvas, "南", textWidth);
+                    break;
+                case 180:
+                    textWidth = textPaint.measureText("225");
+                    drawText(canvas, "西", textWidth);
+                    break;
+                case 225:
+                    textWidth = textPaint.measureText("西");
+                    drawText(canvas, "西", textWidth);
+                    break;
+                case 270:
+                    textWidth = textPaint.measureText("北");
+                    drawText(canvas, "北", textWidth);
+                    canvas.drawLine(0, -compassRadius + tickLength + textHeight + 10, -textWidth / 3, -computeHorizontalScrollExtent() + tickLength + textHeight + 30, tickPaint);
+                    canvas.drawLine(0, -compassRadius + tickLength + textHeight + 10, -textWidth / 3, -compassRadius + tickLength + textHeight + 30, tickPaint);
+                    break;
+                default:
+                    break;
+            }
+            canvas.restore();
+        }
+    }
+
+    private void drawText(Canvas canvas, String text, float textWidth) {
+        canvas.drawText(text, -(textWidth / 2), -compassRadius + tickLength + textHeight, textPaint);
     }
 }
--- Compass/app/src/main/java/com/android/compass/app/MainActivity.java
+++ Compass/app/src/main/java/com/android/compass/app/MainActivity.java
@@ -1,20 +1,121 @@
 package com.android.compass.app;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import cn.waps.AppConnect;
 
+import java.util.Timer;
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    RelativeLayout compass_layout;
+    TextView textView;
+    ImageView imageView;
+    SensorManager sensorManager;
+    SensorEventListener listener;
+    Sensor sensor1;
+    Sensor sensor2;
+    Timer updateTimer;
+    float[] accelerometerValues = new float[3];
+    float[] magneticFieldValues = new float[3];
+    float[] values = new float[3];
+    float[] Re = new float[9];
+    float startDegree = 0f;
+    float pivotX;
+    float pivotY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LinearLayout layout = (LinearLayout) findViewById(R.id.advertise_layout);
-        ImageView imageView = (ImageView) findViewById(R.id.compass_image);
-        AppConnect.getInstance(this).showBannerAd(this, layout);
+        AppConnect.getInstance("70f005ce4d7cdebfd4ed3ae42ba3b3fc", "default", this).showBannerAd(this, layout);
+        compass_layout = (RelativeLayout) findViewById(R.id.compass_layout);
+//        CompassView compassView = new CompassView(this);
+//        compass_layout.addView(compassView);
 
+        textView = (TextView) findViewById(R.id.text);
+//        textView.setText("North");
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor1 = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor2 = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        imageView.measure(0, 0);
+        pivotX = imageView.getMeasuredWidth() / 2 + imageView.getX();
+        pivotY = imageView.getMeasuredHeight() / 2 + imageView.getY();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                    magneticFieldValues = sensorEvent.values;
+                }
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    accelerometerValues = sensorEvent.values;
+                }
+                updateUI();
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(listener, sensor1, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(listener, sensor2, SensorManager.SENSOR_DELAY_NORMAL);
+//        updateTimer = new Timer("updateUI");
+//        updateTimer.scheduleAtFixedRate(new TimerTask() {
+//            @Override
+//            public void run() {
+//                updateUI();
+//            }
+//        }, 0, 50);
+
+    }
+
+    private void rotateImage(float fromDegree, float toDegree) {
+
+        Animation rotate = new RotateAnimation(fromDegree, toDegree, pivotX, pivotY);
+        imageView.setAnimation(rotate);
+        rotate.setDuration(100);
+        rotate.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(listener);
+        updateTimer.cancel();
+        super.onPause();
+    }
+
+    private void updateUI() {
+
+        SensorManager.getRotationMatrix(Re, null, accelerometerValues, magneticFieldValues);
+        SensorManager.getOrientation(Re, values);
+        values[0] = (float) Math.toDegrees(values[0]);
+        rotateImage(startDegree, values[0]);
+        startDegree = values[0];
     }
 }
