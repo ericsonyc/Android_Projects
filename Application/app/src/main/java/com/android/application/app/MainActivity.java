67ca62bc03416125907a5d0b8b6759bbfb2b04b4
ericsonyc <1220246394@qq.com>
2015-12-17 13:24:19 +0800

    commit an android project

---
+++ Application/app/src/main/java/com/android/application/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.android.application.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button btn1;
+    private Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn1) {
+
+        } else if (v == btn2) {
+
+        }
+    }
+}
