1b4b92d9518cbce2aac001fa6cf058e38496615c
ericsonyc <1220246394@qq.com>
2016-01-18 15:53:47 +0800

    update

---
+++ AndroidExercises/app/src/main/java/com/android/androidexercises/app/LinearLayoutActivity.java
@@ -0,0 +1,25 @@
+package com.android.androidexercises.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ericson on 2016/1/18 0018.
+ */
+public class LinearLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear4);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
